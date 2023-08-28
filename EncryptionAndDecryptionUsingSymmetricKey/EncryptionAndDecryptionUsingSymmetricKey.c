96927a6f9a03074f8e27c432b83f4987caf65f48
Jashan-panwa <114824486+Jashan-panwa@users.noreply.github.com>
2023-08-28 12:16:34 +0530

    indentation corrected

--- EncryptionAndDecryptionUsingSymmetricKey/EncryptionAndDecryptionUsingSymmetricKey.c
+++ EncryptionAndDecryptionUsingSymmetricKey/EncryptionAndDecryptionUsingSymmetricKey.c
@@ -6,7 +6,7 @@ int main(){
     printf("--> symmetric key means , same key is used for both encryption and decryption\n");
 /*------VARIABLES USED IN THE CODE-------*/    
     int key;              /*-used to get key from user-*/
-	int i;                /*-used in functioning of loops-*/
+    int i;                /*-used in functioning of loops-*/
     int usedkey;          /*-key used for alphabets-*/
     int usednumkey;       /*-key used for integers-*/
     int unlock;           /*-used to chceck the value of key at decryption-*/
@@ -19,15 +19,15 @@ int main(){
 /*------CODE TO GET THE KEY-------*/    
     printf("\nPlease input the key for encryption and decryption (must be an integer): ");
     fflush(stdin);
-	scanf("%d",&key);
+    scanf("%d",&key);
  /*------ADJUSTING KEY SIZE FOR ALPHABETS-------*/   
     if(key>26 && key%26!=0){usedkey=key%26;}
     else if(key%26==0){usedkey=key/26;}
     else{usedkey=key;}
 /*------ADJUSTING KEY SIZE FOR NUMBERS-------*/    
     if(key>10 && key%10!=0){usednumkey=key%10;}
-	else if(key%10==0){usednumkey=key/10;}
-	else{usednumkey=key;}	
+    else if(key%10==0){usednumkey=key/10;}
+    else{usednumkey=key;}	
 /*------CODE WHICH IS DOING ENCRYPTION-------*/  
     for( i=0;str[i];i++){
       if(str[i]==' '){continue;}/*--FOR SPACES--*/
@@ -35,11 +35,11 @@ int main(){
     		if(str[i]+usedkey>'z'){str[i]=str[i]+usedkey-26;}
     		else{str[i]=str[i]+usedkey;}
 	   }
-	  else if(str[i]>='A' && str[i]<='Z'){/*--FOR CAPITAL LETTERS--*/
+      else if(str[i]>='A' && str[i]<='Z'){/*--FOR CAPITAL LETTERS--*/
     		if(str[i]+usedkey>'Z'){str[i]=str[i]+usedkey-26;}
     		else{str[i]=str[i]+usedkey;}
 	   } 
-	  else if(str[i]>='0' && str[i]<='9'){/*--FOR SNUMERIC VALUES--*/
+      else if(str[i]>='0' && str[i]<='9'){/*--FOR SNUMERIC VALUES--*/
     		if(str[i]+usednumkey>'9'){str[i]=str[i]+usednumkey-10;}
     		else{str[i]=str[i]+usednumkey;}
 	   } 
@@ -53,9 +53,7 @@ int main(){
     if(sample=='y'||sample=='Y' ||sample=='yes'|| sample=='YES'|| sample=='Yes'){stepup++;}
     else if(sample=='n'||sample=='N' ||sample=='no'|| sample=='NO'|| sample=='No'){printf("ok , as you wish, we will stop the code here. (press any key to quit)");getch();}
     else{printf("invalid respond , we will consider this as NO. (press any key to quit)",sample);getch();}
-    
-    
-    
+       
     
 /*------CODE WHICH ASKING FOR CORRECT KEY-------*/    
     if(stepup>0){
@@ -76,20 +74,19 @@ int main(){
 	}    
 /*------CODE WHICH IS DOING DECERYPTION-------*/
     if(stepup>1){ 
-	   printf("The Decrypted Data is as follows:\n--> ");
-	   
-	   for( i=0;str[i];i++){
+	 printf("The Decrypted Data is as follows:\n--> "); 
+	 for( i=0;str[i];i++){
       	 if(str[i]==' '){continue;}/*--FOR SPACES--*/
          else if(str[i]>='a' && str[i]<='z'){/*--FOR SMALL LETTERS--*/
     	 	  if(str[i]-usedkey<'a'){str[i]=str[i]-usedkey+26;}
-    	      else{str[i]=str[i]-usedkey;}
+    	          else{str[i]=str[i]-usedkey;}
 	       }
-	     else if(str[i]>='A' && str[i]<='Z'){/*--FOR CAPITAL LETTERS--*/
-    	      if(str[i]-usedkey<'A'){str[i]=str[i]-usedkey+26;}
-    	      else{str[i]=str[i]-usedkey;}
+	 else if(str[i]>='A' && str[i]<='Z'){/*--FOR CAPITAL LETTERS--*/
+    	         if(str[i]-usedkey<'A'){str[i]=str[i]-usedkey+26;}
+    	         else{str[i]=str[i]-usedkey;}
 	       } 
-	     else if(str[i]>='0' && str[i]<='9'){/*--FOR SNUMERIC VALUES--*/
-    	      if(str[i]-usednumkey<'0'){str[i]=str[i]-usednumkey+10;}
+	 else if(str[i]>='0' && str[i]<='9'){/*--FOR SNUMERIC VALUES--*/
+    	          if(str[i]-usednumkey<'0'){str[i]=str[i]-usednumkey+10;}
     		  else{str[i]=str[i]-usednumkey;}
 	       } 
 	   } 
